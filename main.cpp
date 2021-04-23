6910a5d5e082a02aa018c08915607df5e7b9c79d
Evgeniy <k.o.k.o.1998@mail.ru>
2021-04-23 12:42:36 +0300

    Добавлен запуск через внешние аргументы, добита многопоточность
    Тесты+

--- main.cpp
+++ main.cpp
@@ -1,26 +1,63 @@
 #include "Client.hpp"
+#include "Starter.hpp"
 #include <thread>
+
+struct TestingCtx
+{
+    std::size_t threads_size = 0;
+    int clients_size = 0;
+    double upped_bound_timer = 0;
+};
+
+void StartPerformanceTesting(ClientContext& ctx, TestingCtx& testing_ctx);
 
 int main(int argc, char* argv[])
 {
-    int threads_size = 2;
-    int clients_size = 5;
-    int upped_bound_timer = 2;
+    Starter starter("SOCKS5-Performance-Testing");
+    starter.AddArgument("help", "Arguments description");
+    starter.AddArgument("p_ip","Proxy ip", std::string{"127.0.0.1"});
+    starter.AddArgument("tp_ip","Third party ip", std::string{"127.0.0.1"});
 
-    ClientContext ctx{.timer = Timer(upped_bound_timer)};
+    starter.AddRequiredArgument<std::uint16_t>("p_port","Proxy port");
+    starter.AddRequiredArgument<std::uint16_t>("tp_port","Third party port");
+    starter.AddRequiredArgument<std::size_t>("threads","Threads number");
+    starter.AddRequiredArgument<int>("clients","Clients number");
+    starter.AddRequiredArgument<double>("limit","Time limit for testing (seconds)");
+    starter.AddRequiredArgument<std::size_t>("bsize","Testing buffer size");
 
-    ctx.proxying_info.proxy.ip = "127.0.0.1";
-    ctx.proxying_info.third_party.ip = "127.0.0.1";
+    starter.ParseArguments(argc,argv);
 
-    ctx.proxying_info.proxy.port = 5007;
-    ctx.proxying_info.third_party.port = 6000;
+    if(!starter.Failed())
+    {
+        TestingCtx testing_ctx;
 
-    ctx.testing_buffer_size = 1024;
+        starter.GetArgValue("threads", testing_ctx.threads_size);
+        starter.GetArgValue("clients", testing_ctx.clients_size);
+        starter.GetArgValue("limit", testing_ctx.upped_bound_timer);
 
+        ClientContext ctx{.timer = Timer(testing_ctx.upped_bound_timer)};
+
+        starter.GetArgValue("p_ip", ctx.proxying_info.proxy.ip);
+        starter.GetArgValue("tp_ip", ctx.proxying_info.third_party.ip);
+
+        starter.GetArgValue("p_port", ctx.proxying_info.proxy.port);
+        starter.GetArgValue("tp_port", ctx.proxying_info.third_party.port);
+
+        starter.GetArgValue("bsize", ctx.testing_buffer_size);
+
+        StartPerformanceTesting(ctx,testing_ctx);
+
+    }
+
+    return 0;
+}
+
+void StartPerformanceTesting(ClientContext& ctx, TestingCtx& testing_ctx)
+{
     std::vector<Client> clients;
-    clients.reserve(clients_size);
+    clients.reserve(testing_ctx.clients_size);
 
-    for(int i = 0; i < clients_size; i++)
+    for(int i = 0; i < testing_ctx.clients_size; i++)
     {
         clients.emplace_back(Client{ctx,i});
     }
@@ -30,26 +67,35 @@ int main(int argc, char* argv[])
         client.Connect();
     }
 
+    boost::asio::signal_set sigs{ctx.ctx,SIGINT,SIGTSTP, SIGTERM};
+    sigs.async_wait([&](boost::system::error_code error,int signal_number)
+    {
+        if(!error)
+        {
+            ctx.ctx.stop();
+        }
+    });
+
     std::vector<std::thread> threads;
-    threads.reserve(threads_size);
+    threads.reserve(testing_ctx.threads_size);
 
     auto task = [&]()
     {
         ctx.ctx.run();
     };
 
-    for(int i = 0; i < threads_size; i++)
+    std::cout << "Running in " << testing_ctx.threads_size << " threads\n";
+
+    for(int i = 0; i < testing_ctx.threads_size; i++)
     {
         threads.emplace_back(std::thread{task});
     }
 
-    for(auto& th : threads)
+    for(auto& thread : threads)
     {
-        th.join();
+        thread.join();
     }
 
     ctx.timer.PrintPerformanceInfo();
-
-    return 0;
 }
 
